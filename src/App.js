285bbd5fe8d7b11ed76c225be9ef2cb7298b9ded
Muhammad <muhammadmahmudov21@yandex.ru>
2021-10-17 20:31:08 +0300

    v 1.0

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import Postlist from "./components/Postlist";
 import Mybutton from "./components/UI/button/Mybutton";
 import Loader from "./components/UI/Loader/Loader";
 import MyModal from "./components/UI/MyModal/MyModal";
+import { useFetching } from "./hooks/useFetching";
 import { usePosts } from "./hooks/usePosts";
+import { getPagesArray, getPagesCount } from "./utils/pages";
 
 
 
@@ -15,21 +17,25 @@ function App() {
   const [posts,setPosts]= useState([]);
   const [filter,setFilter] = useState({sort:'', query:''})
   const [modal,setModal] = useState(false);
+  const [limit,setLimit] = useState(10);
+  const [page,setPage] = useState(1);
   const sortedAndSearchPosts = usePosts(posts,filter.sort,filter.query);
-  const [isPostLoading,setIsPostLoading] = useState(false);
+  const [totalPages,setTotalPages] = useState(0);
 
-  async function fetchPosts() {
-    setIsPostLoading(true);
-    setTimeout(async () => {
-      const posts = await PostService.getAll();
-      setPosts(posts)
-      setIsPostLoading(false);
-    },1000) 
-  }
+  const [fetchPosts, isPostLoading,postError] = useFetching(async () => { 
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data)
+    const totalCount = response.headers['x-total-count'];
+    setTotalPages(getPagesCount(totalCount,limit));
+  });
+
+  let pagesArray = getPagesArray(totalPages)
+
 
   useEffect(() => {
     fetchPosts();
-  }, [])
+  }, [page])
+
 const createPost = (newPost) => {
   setPosts([...posts,newPost]);
   setModal(false);
@@ -40,6 +46,9 @@ const removePost = (post) => {
   setPosts(posts.filter(p => p.id !== post.id));
 }
 
+const changePage = (page) => {
+  setPage(page)
+}
   return (
     <div className="App">
       <Mybutton style={{marginTop:30}} onClick={() => setModal(true)}>
@@ -54,11 +63,25 @@ const removePost = (post) => {
         setFilter={setFilter}
       />
       {
+      postError && 
+        <h1>Произошла ошибка ${postError}</h1>
+      }
+      {
         isPostLoading
           ? <div style={{display:'flex',justifyContent:'center',marginTop:50}}><Loader /></div> 
           : <Postlist remove={removePost} posts={sortedAndSearchPosts} title="Посты про Js"/>
       }
-
+      <div className="page__wrapper">
+        {pagesArray.map(p => 
+          <span
+            onClick={() => changePage(p)} 
+            key={p}  
+            className={page === p ? 'page page__current':'page'}
+          >
+            {p}
+          </span>
+        )}
+      </div>
     </div>
   );
 }
