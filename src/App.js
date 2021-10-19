c8ec4743a2d94446299be0762fe4b661313b1f41
Muhammad <muhammadmahmudov21@yandex.ru>
2021-10-19 21:00:04 +0300

    [+] pagination component

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ import Postlist from "./components/Postlist";
 import Mybutton from "./components/UI/button/Mybutton";
 import Loader from "./components/UI/Loader/Loader";
 import MyModal from "./components/UI/MyModal/MyModal";
+import Pagination from "./components/UI/pagination/Pagination";
+
+
 import { useFetching } from "./hooks/useFetching";
 import { usePosts } from "./hooks/usePosts";
-import { getPagesArray, getPagesCount } from "./utils/pages";
+import {getPagesCount} from "./utils/pages";
 
 
 
@@ -21,16 +24,12 @@ function App() {
   const [page,setPage] = useState(1);
   const sortedAndSearchPosts = usePosts(posts,filter.sort,filter.query);
   const [totalPages,setTotalPages] = useState(0);
-
   const [fetchPosts, isPostLoading,postError] = useFetching(async () => { 
     const response = await PostService.getAll(limit, page);
     setPosts(response.data)
     const totalCount = response.headers['x-total-count'];
     setTotalPages(getPagesCount(totalCount,limit));
   });
-
-  let pagesArray = getPagesArray(totalPages)
-
 
   useEffect(() => {
     fetchPosts();
@@ -71,17 +70,7 @@ const changePage = (page) => {
           ? <div style={{display:'flex',justifyContent:'center',marginTop:50}}><Loader /></div> 
           : <Postlist remove={removePost} posts={sortedAndSearchPosts} title="Посты про Js"/>
       }
-      <div className="page__wrapper">
-        {pagesArray.map(p => 
-          <span
-            onClick={() => changePage(p)} 
-            key={p}  
-            className={page === p ? 'page page__current':'page'}
-          >
-            {p}
-          </span>
-        )}
-      </div>
+      <Pagination totalPages={totalPages} changePage={changePage} page={page}/>
     </div>
   );
 }
