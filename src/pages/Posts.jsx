957e592768737bdfbe3cd6dd2a08cf7fd6e66ae2
Muhammad <muhammadmahmudov21@yandex.ru>
2021-10-21 22:03:06 +0300

    [+*] endless feed

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from "react";
+import React,{useState,useEffect,useRef} from "react";
 import PostService from "../API/PostService";
 import PostFilter from "../components/PostFilter";
 import Postform from "../components/Postform";
@@ -7,9 +7,11 @@ import Mybutton from "../components/UI/button/Mybutton";
 import Loader from "../components/UI/Loader/Loader";
 import MyModal from "../components/UI/MyModal/MyModal";
 import Pagination from "../components/UI/pagination/Pagination";
+import MySelect from "../components/UI/select/MySelect";
 
 
 import { useFetching } from "../hooks/useFetching";
+import { useObserver } from "../hooks/useObserver";
 import { usePosts } from "../hooks/usePosts";
 import {getPagesCount} from "../utils/pages";
 
@@ -23,16 +25,22 @@ function Posts() {
   const [page,setPage] = useState(1);
   const sortedAndSearchPosts = usePosts(posts,filter.sort,filter.query);
   const [totalPages,setTotalPages] = useState(0);
+  const lastElement = useRef();
+  const observer = useRef();
   const [fetchPosts, isPostLoading,postError] = useFetching(async () => { 
     const response = await PostService.getAll(limit, page);
-    setPosts(response.data)
+    setPosts([...posts, ...response.data]) 
     const totalCount = response.headers['x-total-count'];
     setTotalPages(getPagesCount(totalCount,limit));
   });
 
+  useObserver(lastElement, page < totalPages, isPostLoading, () => {
+    setPage(page+1)
+  })
+
   useEffect(() => {
-    fetchPosts();
-  }, [page])
+    fetchPosts(limit,page);
+  }, [page,limit])
 
 const createPost = (newPost) => {
   setPosts([...posts,newPost]);
@@ -60,14 +68,26 @@ const changePage = (page) => {
         filter={filter} 
         setFilter={setFilter}
       />
+      <MySelect
+        value = {limit}
+        onChange = {value  => setLimit(value)}
+        defaultValue = "Кол-во элементов на странице"
+        options = {[
+          {value:5, name:'5',},
+          {value:10, name:'10',},
+          {value:'25', name:'25',},
+          {value:'-1', name:'показать все',}
+        ]}
+      />
       {
       postError && 
         <h1>Произошла ошибка ${postError}</h1>
       }
+      <Postlist remove={removePost} posts={sortedAndSearchPosts} title="Посты про Js"/>
+      <div ref={lastElement} style={{height:20,background:'red'}}/>
       {
-        isPostLoading
-          ? <div style={{display:'flex',justifyContent:'center',marginTop:50}}><Loader /></div> 
-          : <Postlist remove={removePost} posts={sortedAndSearchPosts} title="Посты про Js"/>
+        isPostLoading && 
+          <div style={{display:'flex',justifyContent:'center',marginTop:50}}><Loader /></div> 
       }
       <Pagination totalPages={totalPages} changePage={changePage} page={page}/>
     </div>
